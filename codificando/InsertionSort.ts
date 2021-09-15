558f70afda24a8e43ced6919b281af9a1071d8ef
Vagner Bellacosa <vagnerbellacosa@Gmail.com>
2021-09-14 21:41:30 -0300

    Create InsertionSort.ts

---
+++ codificando/InsertionSort.ts
@@ -0,0 +1,16 @@
+function insertionSort(list: number[]): number[] {
+  const clonedList = [...list]
+
+  for (let increment = 1; increment < clonedList.length; increment++) {
+    const currentValue = clonedList[increment]
+    let j = increment - 1
+
+    while (j >= 0 && clonedList[j] > currentValue) {
+      clonedList[j + 1] = clonedList[j]
+      j -= 1
+    }
+    clonedList[j + 1] = currentValue
+  }
+
+  return clonedList
+}
